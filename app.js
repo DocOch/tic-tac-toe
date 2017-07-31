e3004ce5316102d8c1978fed53c1ca50535d0b5b
DocOch <Ochmanowicz@gmail.com>
2017-07-31 14:21:03 -0400

    places pieces and checks for winner

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-var board = [[ , , ],[ , , ],[ , , ]];
+class Board {
+  constructor(){
+	this.view = [[ , , ],[ , , ],[ , , ]];
+	this.turn = 'X'
+  }
+	this.check = function() {
+	  for(var i = 0; i<3; i++){
+	  	if(this.view[i][0] !== ' ' && this.view[i][0] === this.view[i][1] && this.view[i][1] === this.view[i][2]){
+	  	  return `${this.view[i][0]} WINS!!`
+	  	} else if(this.view[0][i] !== ' ' && this.view[0][i] === this.view[1][i] && this.view[1][i] === this.view[2][i]){
+	  	  return `${this.view[i][0]} WINS!!`
+	  	}
+	  }
+	  if(this.view[0][0] !== ' ' && this.view[0][0] === this.view[1][1] && this.view[1][1] === this.view[2][2]){
+	  	return `${this.view[0][0]} WINS!!`
+	  } else if(this.view[2][0] !== ' ' && this.view[2][0] === this.view[1][1] && this.view[1][1] === this.view[0][2]){
+	  	return `${this.view[2][0]} WINS!!`
+	  }
+	}
+	this.placePiece = function(row,col){
+	  this.view[row][col] = this.turn
+	  if(this.turn === 'X'){
+		this.turn = 'O'
+	  } else {
+		this.turn = 'X'
+	  }
+	  this.check()
+  }
+} 
+
